d7cb427531856a94dd56cc7977498c0d50e8cdc0
VladimirSemen2022 <107301979+VladimirSemen2022@users.noreply.github.com>
2023-06-20 19:18:10 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using HomeWork_3_WPF.Models;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Effects;
+
+namespace HomeWork_3_WPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void calendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DateTime? selectedDate = calendar1.SelectedDate;
+            MoonFases moonFases = new MoonFases(selectedDate??DateTime.Now);
+            ShowMoonFases(moonFases);
+        }
+
+        public void ShowMoonFases(MoonFases moonFase)
+        {
+            var eff = new DropShadowEffect();
+            ThicknessAnimation EarthAnimation = new ThicknessAnimation();
+            EarthAnimation.Duration = TimeSpan.FromMilliseconds(800);
+            EarthAnimation.FillBehavior = FillBehavior.HoldEnd;
+
+            ThicknessAnimation MoonAnimation = new ThicknessAnimation();
+            MoonAnimation.Duration = TimeSpan.FromMilliseconds(800);
+            MoonAnimation.FillBehavior = FillBehavior.HoldEnd;
+
+            if (moonFase.MoonDay < 16)
+            {
+                eff.Direction = 0;
+                eff.Opacity = 0.9;
+                eff.BlurRadius = 15;
+                Earth_Shadow.Direction = eff.Direction;
+                EarthAnimation.To = new Thickness(-700 + (500 * (moonFase.MoonDay -1) / 14), -230, 0, 0);
+                MoonAnimation.To = new Thickness(-700 + (700 * (moonFase.MoonDay-1) / 14), -230, 0, 0);
+            }
+            else
+            {
+                eff.Direction = 180;
+                eff.Opacity = 0.9;
+                eff.BlurRadius = 15;
+                Earth_Shadow.Direction = eff.Direction;
+                EarthAnimation.To = new Thickness(200 + (500 * (moonFase.MoonDay - 15) / 15), -230, 0, 0);
+                MoonAnimation.To = new Thickness(700 * (moonFase.MoonDay - 15) / 15, -230, 0, 0);
+            }
+            Fase.Text = moonFase.MoonFase;
+            Day.Text = moonFase.MoonDay.ToString();
+
+            Storyboard.SetTarget(EarthAnimation, Earth);
+            Storyboard.SetTargetProperty(EarthAnimation, new PropertyPath(MarginProperty));
+            Storyboard EarthStoryboard = new Storyboard();
+            EarthStoryboard.Children.Add(EarthAnimation);
+            EarthStoryboard.Begin();
+
+            Storyboard.SetTarget(MoonAnimation, Moon);
+            Storyboard.SetTargetProperty(MoonAnimation, new PropertyPath(MarginProperty));
+            Storyboard MoonStoryboard = new Storyboard();
+            MoonStoryboard.Children.Add(MoonAnimation);
+            MoonStoryboard.Begin();
+        }
+    }
+}
---
+++ Models/MoonFases.cs
@@ -0,0 +1,85 @@
+﻿using MaterialDesignThemes.Wpf.Converters.CircularProgressBar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_3_WPF.Models
+{
+    public enum Fases
+    {
+        New_Moon,
+        Waxing_Crescent,
+        First_Quarter,
+        Waxing_Gibbous,
+        Full_Moon,
+        Waning_Gibbous,
+        Third_Quarter,
+        Waning_Crescent,
+        End_Moon_Month
+    }
+    public class MoonFases
+    {
+        public int MoonDay { get; set; }
+        public String MoonFase { get; set; }
+
+        public MoonFases()
+        {
+                MoonDay = 0;
+                MoonFase = string.Empty;
+        }
+
+
+        public MoonFases(int moonDay)
+        {
+            MoonDay = moonDay;
+            if (MoonDay == 1)
+                MoonFase = Fases.New_Moon.ToString();
+            else if (MoonDay > 1 && MoonDay < 7)
+                MoonFase = Fases.Waxing_Crescent.ToString();
+            else if (MoonDay == 7)
+                MoonFase = Fases.First_Quarter.ToString();
+            else if (MoonDay > 7 && MoonDay < 15)
+                MoonFase = Fases.Waxing_Gibbous.ToString();
+            else if (MoonDay == 15)
+                MoonFase = Fases.Full_Moon.ToString();
+            else if (MoonDay > 15 && MoonDay < 23)
+                MoonFase = Fases.Waning_Gibbous.ToString();
+            else if (MoonDay == 23)
+                MoonFase = Fases.Third_Quarter.ToString();
+            else if (MoonDay > 23 && MoonDay < 30)
+                MoonFase = Fases.Waning_Crescent.ToString();
+            else
+                MoonFase = Fases.End_Moon_Month.ToString();
+        }
+
+        public MoonFases(DateTime date)
+        {
+            int day = date.Day;
+            int month = date.Month;
+            int moonDay = date.Year % 19 + 1;
+            MoonDay = ((moonDay * 11) - 14 + day + month) % 30;
+            if (MoonDay == 0)
+                MoonDay = 30;
+            if (MoonDay == 1)
+                MoonFase = Fases.New_Moon.ToString();
+            else if (MoonDay > 1 && MoonDay < 7)
+                MoonFase = Fases.Waxing_Crescent.ToString();
+            else if (MoonDay > 6 && MoonDay < 9)
+                MoonFase = Fases.First_Quarter.ToString();
+            else if (MoonDay > 8 && MoonDay < 14)
+                MoonFase = Fases.Waxing_Gibbous.ToString();
+            else if (MoonDay > 13 && MoonDay < 17)
+                MoonFase = Fases.Full_Moon.ToString();
+            else if (MoonDay > 16 && MoonDay < 22)
+                MoonFase = Fases.Waning_Gibbous.ToString();
+            else if (MoonDay > 21 && MoonDay < 24)
+                MoonFase = Fases.Third_Quarter.ToString();
+            else if (MoonDay > 23 && MoonDay < 30)
+                MoonFase = Fases.Waning_Crescent.ToString();
+            else
+                MoonFase = Fases.End_Moon_Month.ToString();
+        }
+    }
+}
